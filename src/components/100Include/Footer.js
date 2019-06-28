c700798d4ffc5bcaaae9c23b92a2d3ebc4aa2c19
Henry Cheung <henrycheung@joyaether.com>
2019-06-28 18:25:29 +0800

    redo header

--- src/components/100Include/Footer.js
+++ src/components/100Include/Footer.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { withTranslation } from 'react-i18next';
 
 function Footer(props) {
+	const { i18n } = props;
+
 	return (
-		<div className="stc-footer">123</div >
+		<div className="wrapper-footer">
+			<div className="footer">
+				<ul className="clearfix">
+					<li>
+						<Link to={"/" + i18n.language + "/contact-us"}>联系我们</Link>
+					</li>
+					<li>
+						<Link to={"/" + i18n.language + "/privacy-policy"}>隐私政策</Link>
+					</li>
+					<li>
+						<Link to={"/" + i18n.language + "/copyright"}>版权和免责声明</Link>
+					</li>
+				</ul>
+				<div className="copyright">
+					<span>&copy; 2019 Fablead学院。&nbsp;&nbsp;版权所有。</span>
+				</div>
+			</div>
+		</div>
 	);
 }
 
@@ -11,4 +32,4 @@ Footer.propTypes = {
 	html: PropTypes.string
 };
 
-export default Footer;
+export default withTranslation()(Footer);
--- src/components/103Pages/CommonPage.js
+++ src/components/103Pages/CommonPage.js
@@ -1,18 +1,11 @@
+// Essential for all components
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
-import { map } from 'lodash-es';
+
+// Utils
+// import { map } from 'lodash-es';
 import { Carousel } from 'react-responsive-carousel';
-import Footer from '../100Include/Footer';
-
-
-import 'holderjs';
-
-function renderCarouselImage(image) {
-    return <div key={image.url}>
-        <img src={image.url} />
-    </div>;
-}
 
 class CommonPage extends Component {
     constructor(props) {
@@ -20,35 +13,20 @@ class CommonPage extends Component {
     }
 
     render() {
-        let { images, content } = this.props;
-        if (!images || images.length === 0) {
-            images = [{
-                url: 'holder.js/640x480'
-            }, {
-                url: 'holder.js/640x480'
-            }];
-        }
-        if (!content) {
-            content = 'Sample page content';
-        }
-        return <div className="stc-common-page">
 
-
-            <div className="stc-common-page-container">
-                {/*<NavBreadCrumb/>*/}
-                <div className="container">
-                    <Carousel
-                        showThumbs={false}
-                    >
-                        {map(images, renderCarouselImage)}
-                    </Carousel>
-                    <div className="stc-common-page-content" dangerouslySetInnerHTML={{ __html: content }}></div>
+        return (
+            <div>
+                <div className="wrapper-container-main">
+                    <div className="container-main">
+                        123
+                        <Carousel
+                            showThumbs={false}
+                        ></Carousel>
+                    </div>
                 </div>
             </div>
-            <Footer />
-        </div>;
+        )
     }
 }
-
 
 export default withTranslation()(CommonPage);
