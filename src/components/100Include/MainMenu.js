e6090ab24ec2d0d9bd49fd75203338c2da9aada9
Henry Cheung <henrycheung@joyaether.com>
2019-07-03 18:15:42 +0800

    Menu - 12

--- src/components/100Include/MainMenu.js
+++ src/components/100Include/MainMenu.js
@@ -15,10 +15,10 @@ function MainMenu(props) {
                         <ul className="leftOne">
                             <div className="top"></div>
                             <div className="middle">
-                                <li><Link to={"/" + i18n.language + "/why-we-must-act"}>Why We Must Act</Link></li>
-                                <li><Link to={"/" + i18n.language + "/our-mission-and-strategy"}>Our Mission and Strategy</Link></li>
-                                <li><Link to={"/" + i18n.language + "/contact-us"}>Contact Us</Link></li>
-                                <li><Link to={"/" + i18n.language + "/sitemap"}>Sitemap</Link></li>
+                                <li><Link to={"/" + i18n.language + "/why-we-must-act"}><span>Why We Must Act</span></Link></li>
+                                <li><Link to={"/" + i18n.language + "/our-mission-and-strategy"}><span>Our Mission and Strategy</span></Link></li>
+                                <li><Link to={"/" + i18n.language + "/contact-us"}><span>Contact Us</span></Link></li>
+                                <li><Link to={"/" + i18n.language + "/sitemap"}><span>Sitemap</span></Link></li>
                             </div>
                             <div className="bottom"></div>
                         </ul>
@@ -27,10 +27,25 @@ function MainMenu(props) {
                     <li><Link to={"/" + i18n.language + "/hello"} className="resource">Resources<br />For You</Link>
                         <ul className="leftTwo">
                             <div className="top"></div>
-                            <div className="middle">
-                                <li><Link to={"/" + i18n.language + "/primary-school-home"}>Primary School</Link></li>
-                                <li><Link to={"/" + i18n.language + "/secondary-school-home"}>Secondary School</Link></li>
-                                <li><Link to={"/" + i18n.language + "/parents-home"}>Parents</Link></li>
+                            <div className="middle clearfix">
+                                <li>
+                                    <Link to={"/" + i18n.language + "/primary-school-home"}>
+                                        <div className="thumb one"></div>
+                                        <span>Primary School</span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={"/" + i18n.language + "/secondary-school-home"}>
+                                        <div className="thumb two"></div>
+                                        <span>Secondary School</span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={"/" + i18n.language + "/parents-home"}>
+                                        <div className="thumb three"></div>
+                                        <span>Parents</span>
+                                    </Link>
+                                </li>
                             </div>
                             <div className="bottom"></div>
                         </ul>
@@ -40,10 +55,10 @@ function MainMenu(props) {
                         <ul>
                             <div className="top"></div>
                             <div className="middle">
-                                <li><Link to={"/" + i18n.language + "/online-grooming"}>Online Grooming</Link></li>
-                                <li><Link to={"/" + i18n.language + "/sexting"}>Sexting</Link></li>
-                                <li><Link to={"/" + i18n.language + "/personal-privacy"}>Personal Privacy</Link></li>
-                                <li><Link to={"/" + i18n.language + "/legislations"}>Legislations</Link></li>
+                                <li><Link to={"/" + i18n.language + "/online-grooming"}><span>Online Grooming</span></Link></li>
+                                <li><Link to={"/" + i18n.language + "/sexting"}><span>Sexting</span></Link></li>
+                                <li><Link to={"/" + i18n.language + "/personal-privacy"}><span>Personal Privacy</span></Link></li>
+                                <li><Link to={"/" + i18n.language + "/legislations"}><span>Legislations</span></Link></li>
                             </div>
                             <div className="bottom"></div>
                         </ul>
@@ -53,8 +68,8 @@ function MainMenu(props) {
                         <ul>
                             <div className="top"></div>
                             <div className="middle">
-                                <li><Link to={"/" + i18n.language + "/our-research"}>Our Research</Link></li>
-                                <li><Link to={"/" + i18n.language + "/other-findings"}>Other Findings</Link></li>
+                                <li><Link to={"/" + i18n.language + "/our-research"}><span>Our Research</span></Link></li>
+                                <li><Link to={"/" + i18n.language + "/other-findings"}><span>Other Findings</span></Link></li>
                             </div>
                             <div className="bottom"></div>
                         </ul>
@@ -65,9 +80,9 @@ function MainMenu(props) {
                         <ul>
                             <div className="top"></div>
                             <div className="middle">
-                                <li><Link to={"/" + i18n.language + "/events-safer-internet-day-2019"}>Safer Internet Day 2019</Link></li>
-                                <li><Link to={"/" + i18n.language + "/events-smart-netizen-competition"}>"The Smart Netizen" Character Design Competition</Link></li>
-                                <li><Link to={"/" + i18n.language + "/events-training-workshops"}>Training Workshops</Link></li>
+                                <li><Link to={"/" + i18n.language + "/events-safer-internet-day-2019"}><span>Safer Internet Day 2019</span></Link></li>
+                                <li><Link to={"/" + i18n.language + "/events-smart-netizen-competition"}><span>"The Smart Netizen" Character Design Competition</span></Link></li>
+                                <li><Link to={"/" + i18n.language + "/events-training-workshops"}><span>Training Workshops</span></Link></li>
                             </div>
                             <div className="bottom"></div>
                         </ul>
