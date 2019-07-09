3ec70df0fbd171d9f2d9cc66306d7bc29a01af0f
melodyn99 <melodyna@joyaether.com>
2019-07-09 18:37:37 +0800

    made register page

--- src/containers/07Login/LoginRegister.js
+++ src/containers/07Login/LoginRegister.js
@@ -35,7 +35,20 @@ class LoginRegister extends Component {
         super(props);
 
         this.state = {
-            formSubmitted: false
+            formSubmitted: false,
+            tabIndex: 1
+        }
+    }
+
+    componentDidMount = () => {
+        if (this.props.tabIndex === 1) {
+            this.setState({
+                tabIndex: 1
+            })
+        } else if (this.props.tabIndex === 2) {
+            this.setState({
+                tabIndex: 2
+            })
         }
     }
 
@@ -62,34 +75,70 @@ class LoginRegister extends Component {
             //, t, i18n 
         } = this.props;
 
-        return (
-            <Form className="content">    
-                <Grid container spacing={16}>
-                    <Grid item xs={12} className="grid">
-                        <Field name="email" type="text" placeholder="Enter your Email" maxLength="100" style={{'width':'100%'}}/>
-                        {errors.email && touched.email ? <ErrorMessage message={errors.email} /> : null}
+        if (this.state.tabIndex===1) {
+            return (
+                <Form className="content">    
+                    <Grid container spacing={16}>
+                        <Grid item xs={12} className="grid">
+                            <Field name="email" type="text" placeholder="Enter your Email" maxLength="100" style={{'width':'100%'}}/>
+                            {errors.email && touched.email ? <ErrorMessage message={errors.email} /> : null}
+                        </Grid>
+                        <Grid item xs={12} className="grid">
+                            <Field name="password" type="text" placeholder="Enter your password" maxLength="100" style={{'width':'100%'}}/>
+                            {errors.password && touched.password ? <ErrorMessage message={errors.password} /> : null}
+                        </Grid>
+    
+                        <Grid item xs={12} className="grid">
+                            <Button onClick={() =>this.props.history.push()}>Login</Button>
+                        </Grid>
+                        <Grid item xs={12} className="grid">
+                            <Link to={"/"}>Forgot your password?</Link>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} className="grid">
-                        <Field name="password" type="text" placeholder="Enter your password" maxLength="100" style={{'width':'100%'}}/>
-                        {errors.password && touched.password ? <ErrorMessage message={errors.password} /> : null}
+    
+                    {/* <div className="bottomControl clearfix">
+                        <Button className={classes.greyButton}
+                            onClick={() => this.props.history.push('school-course-note')}
+                        >取消</Button>
+                        <span className="right"><Button type="submit" className={classes.blackButton}>确认</Button></span>
+                    </div> */}
+                </Form>
+            )
+        }
+        else {
+            return (
+                <Form className="content">    
+                    <Grid container spacing={16}>
+                        <Grid item xs={12} className="grid">
+                            <Field name="email" type="text" placeholder="Your email" maxLength="100" style={{'width':'100%'}}/>
+                            {errors.email && touched.email ? <ErrorMessage message={errors.email} /> : null}
+                        </Grid>
+                        <Grid item xs={12} className="grid">
+                            <Field name="name" type="text" placeholder="Display name" maxLength="100" style={{'width':'100%'}}/>
+                            {errors.name && touched.name ? <ErrorMessage message={errors.name} /> : null}
+                        </Grid>
+                        <Grid item xs={12} className="grid">
+                            <Field name="password" type="text" placeholder="Your password" maxLength="100" style={{'width':'100%'}}/>
+                            {errors.password && touched.password ? <ErrorMessage message={errors.password} /> : null}
+                        </Grid>
+                        <Grid item xs={12} className="grid">
+                            <Field name="confirmPassword" type="text" placeholder="Confirm password" maxLength="100" style={{'width':'100%'}}/>
+                            {errors.confirmPassword && touched.confirmPassword ? <ErrorMessage message={errors.confirmPassword} /> : null}
+                        </Grid>
+    
+                        <Grid item xs={12} className="grid">
+                            <ul>
+                                <li>Must have at least 8 characters with numbers</li>
+                                <li>Use upper and lower case letters (eg. Aa)</li>
+                            </ul>
+                        </Grid>
+                        <Grid item xs={12} className="grid">
+                            <Button onClick={() =>this.props.history.push()}>Create New Account</Button>
+                        </Grid>
                     </Grid>
-
-                    <Grid item xs={12} className="grid">
-                        <Button onClick={() =>this.props.history.push()}>Login</Button>
-                    </Grid>
-                    <Grid item xs={12} className="grid">
-                        <Link to={"/"}>Forgot your password?</Link>
-                    </Grid>
-                </Grid>
-
-                {/* <div className="bottomControl clearfix">
-                    <Button className={classes.greyButton}
-                        onClick={() => this.props.history.push('school-course-note')}
-                    >取消</Button>
-                    <span className="right"><Button type="submit" className={classes.blackButton}>确认</Button></span>
-                </div> */}
-            </Form>
-        )
+                </Form>
+            )
+        }
     }
 
     handleSubmit = (values, { setFieldError }) => {
@@ -105,8 +154,16 @@ class LoginRegister extends Component {
             email: Yup.string()
                 .email('Email has to be a valid email address')
                 .required('Email is required'),
+            name: Yup.string()
+                .required('Name is required'),
             password: Yup.string()
+                .typeError('Password must be a valid string')
+                .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, "Does not match Password requirements!")
                 .required('Password is required'),
+            confirmPassword: Yup.string()
+                .typeError('Confirm Password must be a valid string')
+                .oneOf([Yup.ref('password'), null], "Does not match with Password!")
+                .required('Confirm Password is required'),
         })
 
         return (
@@ -126,7 +183,17 @@ class LoginRegister extends Component {
                                         component={this.form}
                                     />
                                 </Tab>
-                                <Tab eventKey={2} title="Register"></Tab>
+                                <Tab eventKey={2} title="Register">
+                                    <Formik
+                                        initialValues={{
+                                            email: '',
+                                            password: '',
+                                        }}
+                                        validationSchema={Schema}
+                                        onSubmit={this.handleSubmit}
+                                        component={this.form}
+                                    />
+                                </Tab>
                             </Tabs>
                         </div>
                     </div>
