9dd2debf22f2b0b5a61e08f0a3ebea517353434d
melodyn99 <melodyna@joyaether.com>
2019-07-29 15:48:39 +0800

    working on popup seekhelp

---
+++ src/components/101Popup/SeekHelp.js
@@ -0,0 +1,52 @@
+// Essential for all components
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import { Redirect } from 'react-router';
+// import { Link } from 'react-router-dom';
+import { withTranslation } from 'react-i18next';
+
+// Styling
+
+// Api
+// import { apiAuth } from '../../Api/ApiAuth';
+
+// Redux
+import { connect } from 'react-redux';
+import { login } from '../../Redux/Action/authAction';
+
+// Utils
+
+// Children components
+// import BreadCrumb from '../../../components/100Include/BreadCrumb';
+
+class SeekHelp extends Component {
+    // constructor(props) {
+    //     super(props);
+
+    //     // this.state = {
+    //     //     formSubmitted: false
+    //     // }
+    // }
+
+    render() {
+        // const { classes } = this.props;
+
+        return (
+            <div className="helpBoard">
+                <div className="icon">
+                    hello
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    auth: state.auth
+});
+
+const mapDispatchToProps = dispatch => ({
+    loginP: data => dispatch(login(data))
+});
+
+export default withTranslation()(connect(mapStateToProps, mapDispatchToProps)(SeekHelp));
